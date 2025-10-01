aa1a1d0e81cda427f6437ea4f89f4fb1ba1da97d
Farouk Merawza <merawza.fr@gmail.com>
2025-10-01 21:46:42 +0200

    feat: update telegram worker to use in memory bus

--- Pipster.Workers.Telegram/Program.cs
+++ Pipster.Workers.Telegram/Program.cs
@@ -1,3 +1,6 @@
+using Pipster.Application.Handlers;
+using Pipster.Application.Parsing;
+using Pipster.Infrastructure.Messaging;
 using Pipster.Infrastructure.Telegram;
 using Pipster.Workers.Telegram;
 
@@ -7,10 +10,17 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddTelegramServices(builder.Configuration);
 
 // Add message bus (using existing InMemoryBus)
-builder.Services.AddSingleton<Pipster.Infrastructure.Messaging.IMessageBus, Pipster.Infrastructure.Messaging.InMemoryBus>();
+builder.Services.AddSingleton<IMessageBus, InMemoryBus>();
 
-// Add worker service
+// Add signal parser
+builder.Services.AddSingleton<ISignalParser, RegexSignalParser>();
+
+// Add tenant config provider (in-memory for now)
+builder.Services.AddSingleton<ITenantConfigProvider, InMemoryTenantConfigProvider>();
+
+// Add worker services
 builder.Services.AddHostedService<TelegramWorkerService>();
+builder.Services.AddHostedService<TelegramMessageHandlerWorker>();
 
 var host = builder.Build();
 host.Run();
