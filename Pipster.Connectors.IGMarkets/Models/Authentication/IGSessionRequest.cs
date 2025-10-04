ed90b7106b742344d5552dfd25ad99e9f16c94be
Farouk Merawza <merawza.fr@gmail.com>
2025-10-04 15:16:33 +0200

    feat(ig-markets): add authentication models (session request/response/tokens)

---
+++ Pipster.Connectors.IGMarkets/Models/Authentication/IGSessionRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Pipster.Connectors.IGMarkets.Models.Authentication;
+
+/// <summary>
+/// Request to create a new IG session
+/// </summary>
+public record IGSessionRequest
+{
+    /// <summary>
+    /// IG account username
+    /// </summary>
+    [JsonPropertyName("identifier")]
+    public required string Identifier { get; init; }
+
+    /// <summary>
+    /// IG account password
+    /// </summary>
+    [JsonPropertyName("password")]
+    public required string Password { get; init; }
+}
